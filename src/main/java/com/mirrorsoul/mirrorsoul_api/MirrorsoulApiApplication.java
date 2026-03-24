6db503a7027cc0ddaa0de52fcffb7c8bc6cd1e6d
nsh0919 <chelsea4570@naver.com>
2026-03-24 18:29:12 +0900

    [Init] initial commit

---
+++ src/main/java/com/mirrorsoul/mirrorsoul_api/MirrorsoulApiApplication.java
@@ -0,0 +1,13 @@
+package com.mirrorsoul.mirrorsoul_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirrorsoulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirrorsoulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirrorsoul/mirrorsoul_api/MirrorsoulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirrorsoul.mirrorsoul_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrorsoulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

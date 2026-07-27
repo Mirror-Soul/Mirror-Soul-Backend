c2d599c308ba43ead5ae590b40aea6c609e7a337
ljj5616 <ljj10571@gmail.com>
2026-07-27 12:06:40 +0900

    seperate mysql & postgre DB

--- src/main/java/com/mirrorsoul/mirrorsoul_api/config/VectorDataSourceConfig.java
+++ src/main/java/com/mirrorsoul/mirrorsoul_api/config/VectorDataSourceConfig.java
@@ -9,11 +9,30 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
 @ConditionalOnProperty(prefix = "vector.datasource", name = "enabled", havingValue = "true")
 public class VectorDataSourceConfig {
+
+    @Bean(name = "mainDataSourceProperties")
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties mainDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "dataSource")
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource dataSource(
+            @Qualifier("mainDataSourceProperties") DataSourceProperties properties
+    ) {
+        return properties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
 
     @Bean
     @ConfigurationProperties("vector.datasource")
